56f0be9e4f738b7d685fd5cf925dc57f05422f44
Haslinda <you@example.com>
2018-11-13 16:30:07 +0800

    ready for heroku

--- Movie.js
+++ Movie.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const db = 'mongodb://paul:abc123@ds221242.mlab.com:21242/movies';
+const db = 'mongodb://movie:movie123@ds141043.mlab.com:41043/lindamovies';
 
 mongoose
   .connect(
